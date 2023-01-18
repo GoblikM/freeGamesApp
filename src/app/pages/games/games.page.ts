e49fba3602958dfcb897c6d64d149008b0e3b103
GoblikM <goldbachmates@seznam.cz>
2023-01-18 21:16:45 +0100

    basic page with games

--- src/app/pages/games/games.page.ts
+++ src/app/pages/games/games.page.ts
@@ -1,4 +1,7 @@
+import { Observable } from 'rxjs';
+import { Game, GamesService } from './../../services/games.service';
 import { Component, OnInit } from '@angular/core';
+import { LoadingController } from '@ionic/angular';
 
 @Component({
   selector: 'app-games',
@@ -7,9 +10,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GamesPage implements OnInit {
 
-  constructor() { }
+  games: any[]=[];
+
+  //injecting gamesService
+  constructor(private gamesService: GamesService, private loadingCtlr: LoadingController) { }
 
   ngOnInit() {
+    this.initGames();
+  }
+
+  async initGames(){
+
+    const loading = await this.loadingCtlr.create({
+      message: 'Loading games...',
+      spinner: 'crescent',
+      backdropDismiss:false,
+      translucent:true
+
+    })
+    await loading.present();
+
+    this.gamesService.getGames().subscribe(res =>{
+      loading.dismiss();
+      this.games.push(res);
+    });
   }
 
 }
--- src/app/services/games.service.ts
+++ src/app/services/games.service.ts
@@ -1,9 +1,77 @@
+import { environment } from './../../environments/environment';
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+export interface Game {
+  id: number;
+  title: string;
+  thumbnail: string;
+  short_description: string;
+  game_url: string;
+  genre: string;
+  platform: string;
+  publisher: string;
+  developer: string;
+  release_date: string;
+  freetogame_profile_url: string;
+}
+
+
+export interface MinimumSystemRequirements {
+  os: string;
+  processor: string;
+  memory: string;
+  graphics: string;
+  storage: string;
+};
+
+export interface Screenshot {
+  id: number;
+  image: string;
+};
+
+export interface GameDetails {
+  id: number;
+  title: string;
+  thumbnail: string;
+  status: string;
+  short_description: string;
+  description: string;
+  game_url: string;
+  genre: string;
+  platform: string;
+  publisher: string;
+  developer: string;
+  release_date: string;
+  freetogame_profile_url: string;
+  minimum_system_requirements: MinimumSystemRequirements;
+  screenshots: Screenshot[];
+};
+
 
 @Injectable({
   providedIn: 'root'
 })
+
+
+
+
 export class GamesService {
 
-  constructor() { }
+
+  //injecting HttpClient
+  constructor(private http: HttpClient) { }
+
+
+  getGames(): Observable<Game>{
+    const url = 'https://cors-anywhere.herokuapp.com/'+`${environment.baseUrl}/games`;
+     return this.http.get<Game>(url);
+  }
+
+  getGameDetails(id: string):Observable<GameDetails>{
+    const url = 'https://cors-anywhere.herokuapp.com/'+`${environment.baseUrl}/game?id=${id}`;
+    return this.http.get<GameDetails>(url);
+
+  }
 }
